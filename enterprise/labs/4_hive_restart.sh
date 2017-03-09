270e9ae5bbdfb4a7d200c52d9e06e209e66b9d58
michal-sebesta <michal.sebesta@datavard.com>
2017-03-09 10:44:33 +0100

    Update 4_hive_restart.sh

--- enterprise/labs/4_hive_restart.sh
+++ enterprise/labs/4_hive_restart.sh
@@ -1,35 +1,12 @@
 ```
-curl -u michal-sebesta:cloudera 'http://172.31.22.250:7180/api/version'
-v15
+# not exactly script but with using these start and stop commannds with cyclic checking of status started/stopped with some sleep it will do the trick
+
+curl -X POST -u michal-sebesta:cloudera 'http://ip-172-31-22-250:7180/api/v12/clusters/michal-sebesta/services/hive/commands/stop'
 
 
-[root@ip-172-31-22-250 /]# curl -u michal-sebesta:cloudera 'http://172.31.22.250:7180/api/v15/cm/version'
-{
-  "version" : "5.10.0",
-  "buildUser" : "jenkins",
-  "buildTimestamp" : "20170120-1037",
-  "gitHash" : "aa0b5cd5eceaefe2f971c13ab657020d96bb842a",
-  "snapshot" : false"
- } 
-  
-[root@ip-172-31-22-250 /]# curl -u michal-sebesta:cloudera 'http://172.31.22.250:7180/api/v15/users/'
-{
-  "items" : [ {
-    "name" : "admin",
-    "roles" : [ "ROLE_LIMITED" ]
-  }, {
-    "name" : "michal-sebesta",
-    "roles" : [ "ROLE_ADMIN" ]
-  }, {
-    "name" : "minotaur",
-    "roles" : [ "ROLE_CONFIGURATOR" ]
-  } ]
-  
-  
-  [root@ip-172-31-22-250 /]# curl -u michal-sebesta:cloudera 'http://172.31.22.250:7180/api/v15/cm/scmDbInfo'
-{
-  "scmDbType" : "MYSQL",
-  "embeddedDbUsed" : false
-}
+curl -u michal-sebesta:cloudera 'http://ip-172-31-22-250:7180/api/v2/clusters/michal-sebesta/services/hive' | grep  serviceState
+
+
+curl -X POST -u michal-sebesta:cloudera 'http://ip-172-31-22-250:7180/api/v2/clusters/michal-sebesta/services/hive/commands/start'
 
 ```
